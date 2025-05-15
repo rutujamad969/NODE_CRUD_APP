fd77bffe252ac974743c3869833cca399fc60a18
rutujamad969 <madanerutuja8@gmail.com>
2025-05-15 12:15:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const { create } = require('domain')
+const http = require('http')
+const port = 3000 
+
+const server= http.createServer(function(req,res){
+ res.write('hello node')
+  res.end()
+})
+server.listen(port,function(error){
+if (error){
+    console.log('Somethinf went wrong',error)
+}else{
+    console.log('seerver is listening on port' +port)
+
+}
+})
